17a04c8a79c0cc288bb87ef06f1a141c002ba025
Adrian Booth <Adrian.booth17@gmail.com>
2016-01-29 14:42:36 +0000

    Refactor command line reporter

--- reporter.rb
+++ reporter.rb
@@ -6,22 +6,47 @@ require_relative "lib/jellyfish_reporter"
 require_relative "lib/tank_remote"
 
 def set_up
-  @input = File.open('jellyfish_positions.txt').read
-
-  @input_arr = @input.split('\n')[0].split(' ')
-
+  read_file_and_set_data_to_array
   @num_of_fish = @input_arr.count/2
   @tank = Tank.new(@input_arr[0][1].to_i,@input_arr[0][0].to_i)
-
   @input_arr.shift
+  create_new_arrays_of_positions_and_instructions
+end
 
+def read_file_and_set_data_to_array
+  @input = File.open('jellyfish_positions.txt').read
+  @input_arr = @input.split('\n')[0].split(' ')
+end
+
+def create_new_arrays_of_positions_and_instructions
   @fish_positions = []
   @fish_instructions = []
-
-  @input_arr.each_index{|index|
+  @input_arr.each_index do |index|
       @fish_positions << @input_arr[index] if index % 2 == 0
       @fish_instructions << @input_arr[index] if index % 2 != 0
-  }
+  end
+end
+
+def create_new_instances
+  @mover = JellyFishMover.new
+  @remote = TankRemote.new(@tank)
+  @fish = JellyFish.new
+  @reporter = JellyFishReporter.new
+end
+
+def place_and_move_jellyfish
+  @remote.place(@fish)
+  @remote.set_coords(@fish,@fish_positions[@index][0].to_i, @fish_positions[@index][1].to_i,@fish_positions[@index][2])
+  @remote.instruct_to_move(@fish_instructions[@index],@fish,@mover)
+end
+
+def print_output
+  @num_of_fish.times do |i|
+    @index = i
+    create_new_instances
+    place_and_move_jellyfish
+    print "#{@reporter.output(@fish)}\n"
+  end
 end
 
 set_up
@@ -31,13 +56,4 @@ print @input
 puts "\n"
 puts "output"
 puts "-"*20
-@num_of_fish.times do |i|
-  fish = JellyFish.new
-  reporter = JellyFishReporter.new
-  mover = JellyFishMover.new
-  remote = TankRemote.new(@tank)
-  remote.place(fish)
-  remote.set_coords(fish,@fish_positions[i][0].to_i, @fish_positions[i][1].to_i,@fish_positions[i][2])
-  remote.instruct_to_move(@fish_instructions[i],fish,mover)
-  print "#{reporter.output(fish)}\n"
-end
+print_output
